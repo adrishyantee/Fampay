4faa8b43c5082d7b01fe9b0b0ab38e6e173e953d
adrishyantee <adrishyantee@gmail.com>
2022-07-23 11:04:53 +0530

    API Interface

--- FampayProject/app/src/main/java/com/example/fampayproject/api/interfaces/CardGroupsInterface.kt
+++ FampayProject/app/src/main/java/com/example/fampayproject/api/interfaces/CardGroupsInterface.kt
@@ -1,12 +1,12 @@
-package com.example.fampayproject.interfaces
+package com.example.fampayproject.api.interfaces
 
-import com.example.fampayproject.api.models.CardGroups
-import retrofit2.Call
+import com.example.fampayproject.models.CardGroups
 import retrofit2.http.GET
 
 interface CardGroupsInterface {
-    // to get request
 
-    @GET("04a04703-5557-4c84-a127-8c55335bb3b4")
-    fun getData(): Call<List<CardGroups>>
+    //get request
+    @GET("v3/04a04703-5557-4c84-a127-8c55335bb3b4")
+    //we are obtained as a CardGroups instance
+    suspend fun getCardGroups(): CardGroups
 }
